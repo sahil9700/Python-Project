545ece294318d9bb3a2a8e8ba86fdc52f7cd3b63
Sahil Gupta <89833139+sahil9700@users.noreply.github.com>
2022-01-06 21:13:00 +0530

    Update Data_Extraction.py.py

--- Data_Extraction.py.py
+++ Data_Extraction.py.py
@@ -7,76 +7,48 @@ def select_PDF():
     label_file_explorer.configure(text="File Opened: " + filename)
     pdfFileObj = open(filename, 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    ls = []
+    ls=[]
     email = []
     numbers = []
-    time = []
-    date = []
-    # List of 10 largest countries in the world
-    Countries_Largest = ['Russia', 'Canada', 'United States', 'China', 'Brazil', 'Australia', 'India', 'Argentina',
-                         'Kazakhstan', 'Algeria']
-    # A dictionary to store the frequency of each country that occured in our pdf
-    Country_Freq = {'Russia': 0, 'Canada': 0, 'United States': 0, 'China': 0, 'Brazil': 0, 'Australia': 0, 'India': 0,
-                    'Argentina': 0, 'Kazakhstan': 0, 'Algeria': 0}
+    name = []
+    age= []
     print('The data is : ')
 
     # Function to check if the email address is valid or not
     def EmailId_Validity(word):
-        if (word.find('@') >= 0):
-            email.append(word)
+        if (word.find('EmailId')>= 0):
+            ls=word.split(':')
+            email.append(ls[1])
 
     # Function to check if the phone number is valid or not
     def Number_Validity(word):
-        if (word.isdigit() and len(word) == 10):
-            numbers.append(word)
+        if (word.find('PhoneNumber') >= 0):
+            ls=word.split(':')
+            numbers.append(ls[1])
 
     # Function to check if the time is valid or not
-    def Time_Validity(word):
-        if (word.find(':') >= 0):
-            ls = word.split(':')
-            valid_time = True
-            if (len(ls) != 3 and len(ls) != 2):
-                valid_time = False
-            else:
-                for i in ls:
-                    if (i.isdigit() == False):
-                        valid_time = False
-                        break
-            if (valid_time == True):
-                time.append(word)
+    def Name_Validity(word):
+        if (word.find('Name') >= 0):
+            ls= word.split(':')
+            name.append(ls[1])
 
     # Function to check if the date is valid or not
-    def Date_Validity(word):
-        if (word.find('/') >= 0):
-            ls = word.split('/')
-            valid_date = True
-            if (len(ls) != 3):
-                valid_date = False
-            else:
-                for i in ls:
-                    if (i.isdigit() == False):
-                        valid_date = False
-                        break
-                if (valid_date == True):
-                    if (int(ls[0]) > 31 or int(ls[1]) > 12):
-                        valid_date = False
-            if (valid_date == True):
-                date.append(word)
+    def Age_Validity(word):
+        if (word.find('Age') >= 0):
+            ls= word.split(':')
+            age.append(ls[1])
 
     # Function to extract data from a single page
     def Extract(pageObj):
         for line in pageObj.extractText().splitlines():
             line = line.strip()
-            # print(line)
+            #print(line)
             for word in line.split():
 
                 Number_Validity(word)
                 EmailId_Validity(word)
-                Time_Validity(word)
-                Date_Validity(word)
-
-                if word in Country_Freq:
-                    Country_Freq[word] = Country_Freq[word] + 1
+                Name_Validity(word)
+                Age_Validity(word)
 
     # Function to iterate over each page for data extraction in pdf
     for i in range(pdfReader.numPages):
@@ -89,13 +61,12 @@ def select_PDF():
     print('______________________________')
     print('Phone number are', numbers)
     print('______________________________')
-    print('Time are : ', time)
+    print('Names are : ', name)
     print('______________________________')
-    print('Date : ', date)
+    print('Age : ', age)
     print('______________________________')
-    print(Country_Freq)
     pdfFileObj.close()
-    print('\n\n')
+    print('\n')
     print('******THANK YOU*******')
 
 
